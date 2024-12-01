5eb79e3f000c2212dd811f7456decbbeefc9a6f8
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-12-01 15:48:51 +0530

    dataTypes

---
+++ dataType.js
@@ -0,0 +1,22 @@
+// Datatypes
+let name = "zohaib"; //string
+let age = 22; //number
+let isLoggedIn = false; //boolean
+// number => 2 to power 53
+// bigint
+// string => "";
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+
+
+
+
