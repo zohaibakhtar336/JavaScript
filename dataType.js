eb849c3e4d98e870d00e2b3ee20b7d6613fda967
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-12-02 10:59:24 +0530

    dataType summary

--- dataType.js
+++ dataType.js
@@ -1,7 +1,45 @@
+// Types of datatypes
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Rajesh", "Kumar", "Singh"];
+let myObj = {
+    name: "Zohaib",
+    age: 22,
+}
+
+const myFunction = function () {
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+
 // Datatypes
-let name = "zohaib"; //string
-let age = 22; //number
-let isLoggedIn = false; //boolean
+// let name = "zohaib"; //string
+// let age = 22; //number
+// let isLoggedIn = false; //boolean
 // number => 2 to power 53
 // bigint
 // string => "";
