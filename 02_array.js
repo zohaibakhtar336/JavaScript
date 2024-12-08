4d79f280b9c38a527d94065e08ada73cec3d1e18
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-12-08 18:51:58 +0530

    02_array

---
+++ 02_array.js
@@ -0,0 +1,29 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros) // push works on existing array
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros) // concat works on new array
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros] // spread operator
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Zohaib"))
+console.log(Array.from("Zohaib"))
+console.log(Array.from({ name: "zohaib" })) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
