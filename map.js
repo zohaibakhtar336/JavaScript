12fc514fd327789708d05998262013605d626be3
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-12-09 11:29:47 +0530

    map

---
+++ map.js
@@ -0,0 +1,15 @@
+// MAP
+//  Double each number
+const doubledNumbers = numbers.map((num) => num * 2);
+console.log("Doubled Numbers:", doubledNumbers); // [2, 4, 6, 8, 10, 12]
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+    .map((num) => num * 10)
+    .map((num) => num + 1)
+    .filter((num) => num >= 40)
+
+console.log(newNums);
